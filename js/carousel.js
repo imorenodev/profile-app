7f530d8e7d3c4954355a350f19fae2fa9633ead8
imorenodev <imorenodev@gmail.com>
2016-01-31 17:05:35 -0500

    added eventHandlers to carousel arrow buttons

--- js/carousel.js
+++ js/carousel.js
@@ -1,4 +1,15 @@
-$(document).ready(function(){
+var Carousel = (function() {
+
+	var $content = $("[rel=js-carousel] > [rel=js-content]");
+	var $items = $content.children("[rel=js-items]");
+	var $left = $("[rel=js-carousel] > [rel=js-controls] > [rel=js-left]");
+	var $right = $("[rel=js-carousel] > [rel=js-controls] > [rel=js-right]");
+
+
+	var contentWidth = $content.width();
+	var itemsWidth = $items.width();
+	var position = 0;
+	var maxPosition = (itemsWidth - contentWidth);
 
 	function scrollLeft(evt) {
 		evt.preventDefault();
@@ -24,19 +35,15 @@ $(document).ready(function(){
 		$items.css({ left: (-position) + "px" });
 	}
 
-	var $content = $("[rel=js-carousel] > [rel=js-content]");
-	var $items = $content.children("[rel=js-items]");
-	var $left = $("[rel=js-carousel] > [rel=js-controls] > [rel=js-left]");
-	var $right = $("[rel=js-carousel] > [rel=js-controls] > [rel=js-right]");
+    function init() {
+      $left.on("click", scrollLeft);
+	  $right.on("click", scrollRight);
+    }
+    
+    return {
+    	init: init
+    };
 
+})();
 
-	var contentWidth = $content.width();
-	var itemsWidth = $items.width();
-	var position = 0;
-	var maxPosition = (itemsWidth - contentWidth);
-
-	// attach click handlers for the `$left` and `$right` buttons,
-	// that call the `scrollLeft(..)` and `scrollRight(..)` functions,
-	// respectively
-
-});
+$(document).ready(Carousel.init);
