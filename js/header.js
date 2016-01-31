6a3af9b3a913f75286f4487f87ce9ef6a7219aee
imorenodev <imorenodev@gmail.com>
2016-01-31 17:07:25 -0500

    added modal eventhandler to login and register header links

--- js/header.js
+++ js/header.js
@@ -1,1 +1,31 @@
 // put event handlers for header links here
+var Header = (function() {
+
+  function headerLinkClicks(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+
+    var url = $(e.target).attr("href");
+    
+    $.ajax(url, {dataType: "text" })
+      .then(function(contents) {
+        $modal.html(contents).show();
+      });
+  }
+
+  function init() {
+    $modal = $("[rel='js-modal']");
+
+    $("[rel='js-controls']").on("click", "[rel*='js-']", headerLinkClicks );
+  }
+
+  var $modal;
+
+  return {
+    init: init
+  };
+
+})();
+
+$(document).ready(Header.init);
