715bf51bb498ed91fe3392748d0035bf99655680
imorenodev <imorenodev@gmail.com>
2016-01-31 17:19:54 -0500

    refactored using module pattern

--- js/carousel.js
+++ js/carousel.js
@@ -1,15 +1,6 @@
 var Carousel = (function() {
-
-	var $content = $("[rel=js-carousel] > [rel=js-content]");
-	var $items = $content.children("[rel=js-items]");
-	var $left = $("[rel=js-carousel] > [rel=js-controls] > [rel=js-left]");
-	var $right = $("[rel=js-carousel] > [rel=js-controls] > [rel=js-right]");
-
-
-	var contentWidth = $content.width();
-	var itemsWidth = $items.width();
-	var position = 0;
-	var maxPosition = (itemsWidth - contentWidth);
+  
+  var $content, $items, $left, $right, contentWidth, itemsWidth, position, maxPosition;
 
 	function scrollLeft(evt) {
 		evt.preventDefault();
@@ -35,14 +26,25 @@ var Carousel = (function() {
 		$items.css({ left: (-position) + "px" });
 	}
 
-    function init() {
-      $left.on("click", scrollLeft);
-	  $right.on("click", scrollRight);
-    }
-    
-    return {
-    	init: init
-    };
+  function init() {
+    $content = $("[rel=js-carousel] > [rel=js-content]");
+	  $items = $content.children("[rel=js-items]");
+	  $left = $("[rel=js-carousel] > [rel=js-controls] > [rel=js-left]");
+	  $right = $("[rel=js-carousel] > [rel=js-controls] > [rel=js-right]");
+
+
+	  contentWidth = $content.width();
+	  itemsWidth = $items.width();
+	  position = 0;
+	  maxPosition = (itemsWidth - contentWidth);
+
+    $left.on("click", scrollLeft);
+    $right.on("click", scrollRight);
+  }
+  
+  return {
+  	init: init
+  };
 
 })();
 
