9aba6579819ec1003aa8c3848770bf5c8ca31de8
imorenodev <imorenodev@gmail.com>
2016-02-01 13:35:38 -0500

    added modal.js module

---
+++ js/modal.js
@@ -0,0 +1,24 @@
+var Modal = (function() {
+
+	var $modal = $("[rel='js-modal']");
+  
+  function open(url) {
+		var closeButton = "<button rel='js-modal-close'>Close</button>";
+
+	  $.ajax(url, {dataType: "text"})
+	    .then(function(contents) {
+	      $modal.html(contents += closeButton).show();
+
+        init();
+	    });
+  }
+
+  function init() {
+	  $("[rel='js-modal-close']").on("click", function() {
+    	$modal.hide();
+    });
+  }
+  
+  EVT.on("open", open);
+
+})();
